41b4669cf3770ba9e78e8cd0f0f7ff7f2076cdc1
Kaniel <Kaniel.kusi.2022@student.lhschools.org>
2020-03-05 12:14:31 -0500

    OddnessMethodCreated

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -1,8 +1,36 @@
 package com.LickingHieghts;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+       testOdness();
+
+
+        }
+    public static void testOdness(){
+        int number;
+        Scanner keyboard;
+
+        keyboard= new Scanner(System.in);
+        System.out.println("Give me a postive number");
+        number =  keyboard.nextInt();
+        System.out.println("Number is even: "+(number % 2 == 0));
+        System.out.println("Number is odd: "+(number % 2!= 0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
